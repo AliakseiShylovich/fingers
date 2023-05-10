a879ebc848b8451a03f01a4456fa699569a2b5de
AliakseiShylovich <131357751+AliakseiShylovich@users.noreply.github.com>
2023-05-10 14:31:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        n =5;
+        if (n == 1)
+            System.out.println( "Большой палец");
+        else if ( n == 2 )
+            System.out.println( "Указательный палец");
+        else if ( n == 3)
+            System.out.println("Средний палец");
+        else if ( n == 4)
+            System.out.println("Безымянный палец");
+        else if (n == 5)
+            System.out.println("Мезинец");
+    }
+}
